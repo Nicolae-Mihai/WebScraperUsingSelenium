ca88c169fa1181792e811f69b2e20a0a902d3278
Que1Pereza2 <125260665+Que1Pereza2@users.noreply.github.com>
2024-11-06 21:55:22 +0100

    The script is more or less functional
    The script file is a complete mess, but it's more or less functional, still need to comment everything, move things around to make it more legible and to create the function that iterates trough all the movies in the list until it gets to 10k reviews.
    BUT it does function, it's slow as hell but it's functional.

--- README.md
+++ README.md
@@ -11,8 +11,7 @@ After searching on amazon for a product, the web page shows all the products
 related to the search. The page loads images,descriptions, prices and more 
 information that is not relevant for this exercise. The reviews section is 
 located at the bottom of the page.
-Acording to the Exercise requirements the student has to create a scrypt using 
-Python to extract 10.000 reviews and their scores.
+Acording to the Exercise requirements the student has to create a script
     1. The script needs to recieve an input from the user which will become the
 searched term. For example "washing machine".
     2. The program needs to automatically search on amazon for the user input
@@ -21,32 +20,10 @@ and store 10.000 reviews texts and their scores.
 characters that could interfe with the training. The scores have to be reduced
 to two options 0 for negative scores and 1 for positive comments.
     4. Once the comments are processed all the information needs to be stored in
-a CVS with 2 columns. The first column will contain the scores (0 or 1) and the second 
-will contain the cleaned review text.
+a CVS with 2 columns. The first column will contain the scores (0 or 1) and the 
+second will contain the cleaned review text.
 
-
- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-Después de realizar una búsqueda en Amazon, el sistema muestra todos los productos que coinciden con
-las palabras clave seleccionadas por el usuario. Si se selecciona uno de estos productos, Amazon carga
-de manera completa toda la información relacionada con el mismo, incluyendo imágenes, precio y otros
-detalles relevantes. Además, al final de la página del producto, se presentan los comentarios de los
-usuarios que han comprado y utilizado el producto.
-De acuerdo a los requisitos especificados, el estudiante debe crear un script en Python que extraiga
-información de 10,000 comentarios de productos en Amazon. A continuación, se proporciona un resumen
-del procedimiento:
-1. Entrada de Palabra Clave: El script debe solicitar al usuario una palabra clave de búsqueda, por
-ejemplo, "lavadora".
-2. Búsqueda y Extracción de Datos: El programa debe automatizar la búsqueda en Amazon y
-recopilar la información de productos necesaria para alcanzar al menos 10,000 comentarios. Esto
-incluirá la valoración (1 a 5 estrellas) y el texto asociado a cada comentario.
-3. Procesamiento de Datos: El script debe limpiar los comentarios, eliminando caracteres
-irrelevantes que puedan interferir con el entrenamiento. Además, debe reducir las valoraciones de
-estrellas a dos opciones (0 para comentarios negativos y 1 para comentarios positivos).
-4. Almacenamiento en un archivo CSV: Una vez que se han procesado los comentarios, el script
-debe guardar la información en un archivo CSV con dos columnas. La primera columna contendrá
-las valoraciones (0 o 1) y la segunda columna contendrá el texto procesado de la reseña.
-Para llevar a cabo este proyecto, se pueden utilizar bibliotecas como BeautifulSoup para el web scraping
-de Amazon, y herramientas de procesamiento de texto en Python, como nltk o re, para la limpieza de
-texto. Para la reducción de valoraciones de estrellas, puedes utilizar una función que asigne un valor de 0
-o 1 según un umbral predefinido.
+In order to complete the exercise librarys such as BeautifulSoup for web 
+scraping and nltk or re for text cleanup could be used.
+For the score reducction the student can use a function that assigns a value of
+0 or 1 acording to a predefined range.
--- start.py
+++ start.py
@@ -1,2 +1,115 @@
-URL=""
+# This is the script file
+from selenium import webdriver
+import time
+import re
+import random
+import csv
+'''
+    All this variables go together in order to create the search link and then
+the URL is reused to search for the titles.
+    The variables go together like this: 
+     URL + searchPrefix + userInput + searchSufix
+    or like this:
+     URL + searchResultLinks[i]
+'''
+URL="https://www.imdb.com/"
+SEARCH_PREFIX="find/?q="
+USER_INPUT=None
+SEARCH_SUFIX="&ref_=nv_sr_sm"
+WEBSITE_LIST_CLASS="ipc-metadata-list-summary-item__t"
+cleanedReviews=[]
 
+def randomSleep() -> float:
+    return random.uniform(2.0,5.0)
+
+def showSpoilers():
+    spoilers=driver.find_elements("class name","review-spoiler-button")
+
+    for spoiler in spoilers:
+        driver.execute_script("arguments[0].scrollIntoView(true);", spoiler)
+        time.sleep(randomSleep())
+        spoiler.click()
+    
+def getReviews():
+    reviews=driver.find_elements("class name","user-review-item")
+    
+    for review in reviews:
+        tempElem=[]
+        driver.execute_script("arguments[0].scrollIntoView(true);", review)
+        try:
+            tempElem.append(review.find_element("class name","review-rating").text)
+            tempElem.append(review.find_element("class name","ipc-html-content-inner-div").text)
+            cleanedReviews.append(cleanElements(tempElem))
+        except:
+            pass
+        time.sleep(randomSleep())
+
+def cleanElements(elements):
+    cleans = []
+    score = elements[0].rsplit()
+    cleans.append(score[0])
+    cleans.append(re.sub('[^A-Za-z0-9.!? ]+','',elements[1]))
+    return cleans
+
+def writeToCVS():
+    with open("reviews/reviews.cvs","+a",encoding="UTF8") as file:
+        writer=csv.write(file)
+        for row in cleanedReviews:
+            writer.writerow(row)
+    file.close()
+
+moreMovies="/html/body/div[2]/main/div[2]/div[3]/section/div/div[1]/section[2]/div[2]/div/span/button"
+declineCookieButton="/html/body/div[2]/div/div/div[2]/div/button[1]"
+reviewsButton="/html/body/div[2]/main/div/section[1]/div/section/div/div[1]/section[8]/div/div/a/h3"
+showAllReviewsButton="/html/body/div[2]/main/div/section/div/section/div/div[1]/section[1]/div[3]/div/span[2]/button"
+driver=webdriver.Firefox()
+
+# Array in which the list elements are stored
+# searchResultLinks=[]
+
+# USER_INPUT=input("\nWhat would you want to scrape today from " + URL+SEARCH_PREFIX)
+# print('\n Commencing search for "' + USER_INPUT + '".')
+
+# site=URL+SEARCH_PREFIX+USER_INPUT+SEARCH_SUFIX
+site="https://www.imdb.com/title/tt11315808/?ref_=fn_al_tt_1"
+driver.get(site)
+time.sleep(randomSleep())
+
+
+# this part of the code gets all the movies shown on the front page after 
+# clicking more movies button it also declines cookies
+
+driver.find_element("xpath", declineCookieButton).click()
+time.sleep(randomSleep())
+
+# driver.find_element("xpath", moreMovies).click()
+
+# titlesXPath="/html/body/div[2]/main/div[2]/div[3]/section/div/div[1]/section[2]/div[2]/ul"
+
+# time.sleep(randomSleep())
+# movieTable = driver.find_elements("class name","find-title-result")
+
+# for movie in movieTable:
+#     searchResultLinks.append(URL + movie.find_element("class name","ipc-metadata-list-summary-item__t").get_dom_attribute("href"))
+# driver.execute_script('window.scrollTo(0,driver.find_element("xpath",reviewsButton) .offsetTop)')
+reviews = driver.find_element("xpath",reviewsButton)
+driver.execute_script("arguments[0].scrollIntoView(true);", reviews)
+time.sleep(randomSleep())
+reviews.click()
+
+allReviewsButton = driver.find_element("xpath",showAllReviewsButton)
+driver.execute_script("arguments[0].scrollIntoView(true);", allReviewsButton)
+time.sleep(5)
+
+allReviewsButton.click()
+# time.sleep(120)
+
+showSpoilers()
+
+getReviews()
+print("")
+# time.sleep(randomSleep())
+
+
+# driver.get(searchResultLinks[0])
+# driver.quit()
